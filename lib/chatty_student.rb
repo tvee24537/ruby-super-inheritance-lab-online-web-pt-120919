01ad45b2b50f2a25f2102c63a40e94df0c93eb59
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-24 23:01:55 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,22 @@
+class ChattyStudent < Student
+  
+  def hello
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    end
+    
+  def raise_hand
+    i = 0
+    loop do
+      i += 2
+      super
+      puts "Pick me!"
+      if i == 10
+        break       # this will cause execution to exit the loop
+      end
+    end   
+  end
+    
+  
+  
+end
